91a26bcc7c825ee6bf22ebb23ae2211a461de8af
renvar71 <ren.vargas71@gmail.com>
2026-01-04 18:33:10 -0600

    Add mock_data.py with mock match data function

---
+++ mock_data.py
@@ -0,0 +1,10 @@
+# mock_data.py
+
+def get_mock_match(partido_id=None):
+    return {
+        "status": "FINISHED",
+        "home": "América",
+        "away": "Chivas",
+        "score_home": 2,
+        "score_away": 1
+    }
