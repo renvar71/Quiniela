c2cf347a286e9e033828c51884cdeb84cd90d86a
renvar71 <ren.vargas71@gmail.com>
2026-01-07 09:02:33 -0600

    Llave de linea

--- pages/prediccion_partido.py
+++ pages/prediccion_partido.py
@@ -110,7 +110,7 @@ with st.form("form_prediccion"):
         st.write("No se encontró información del partido")
         linea = "N/A"  # para evitar error si no hay valor
     
-    line = st.radio(f"Over / Under total puntos ({linea})", ["Over", "Under"], horizontal=True)
+    line = st.radio(f"Over / Under total puntos ({linea})", ["Over", "Under"], horizontal=True, key="linea")
 
 
     # PREGUNTAS EXTRA
