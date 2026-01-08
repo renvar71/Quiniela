f9f88789b2880c4b312f1a8c97c9d91f7d7f308b
renvar71 <ren.vargas71@gmail.com>
2026-01-08 08:56:16 -0600

    Agregar info.py

--- app.py
+++ app.py
@@ -151,7 +151,8 @@ pages = [
     st.Page("pages/main.py", title="Inicio"),
     st.Page("pages/tabla.py", title="Tabla"),
     st.Page("pages/menu_predicciones.py", title="Mis Predicciones"),
-    st.Page("pages/prediccion_partido.py", title="Predicción")
+    st.Page("pages/prediccion_partido.py", title="Predicción"),
+    st.Page("pages/info.py", title="Invitación")
 ]
 
 pg = st.navigation(pages)
